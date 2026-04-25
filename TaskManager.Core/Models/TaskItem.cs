3d88a32bcf1d6860860a5def50c0e1e730dac565
zenmarmay06 <seyalinr@gmail.com>
2026-04-25 07:48:33 -0700

    feat(model): add PriorityLevel property to Taskitem class

--- TaskManager.Core/Models/TaskItem.cs
+++ TaskManager.Core/Models/TaskItem.cs
@@ -10,10 +10,12 @@ namespace TaskManager.Core.Models
     {
         public int Id { get; set; }
         public string RoomNo { get; set; }
+        public string Priority { get; set; }
         public string AssignedTo { get; set; }
         public DateTime DueDate { get; set; }
         public string Status { get; set; } // "Pending", "In Progress", or "Complete"
         public int UserId { get; set; }
         public string Note { get; set; }
+
     }
 }
